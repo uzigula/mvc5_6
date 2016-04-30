bcadb733ea87a45f75a1393c5b8bc40ce99ab900
Uzi Mamani Fernández <uzi.mamani@outlook.com>
2016-04-30 17:29:20 -0500

    Java Script ejemplos

---
+++ Test_Responsive/Test_Responsive/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_Responsive.Startup))]
+namespace Test_Responsive
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
