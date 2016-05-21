3c4a6ce3fff9dbb5bfc151ccc05727e64ea83961
Uzi Mamani Fernández <uzi.mamani@outlook.com>
2016-05-21 18:48:21 -0500

    Upload Imagenes Ejemplo

---
+++ Controladores/Web.UI/Controllers/ImagenesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Web.UI.Utilidades;
+
+namespace Web.UI.Controllers
+{
+    public class ImagenesController : Controller
+    {
+
+        public ActionResult Subir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Subir(HttpPostedFileBase archivo)
+        {
+            string nombreArchivo =
+                archivo.FileName.ObtenerMD5()
+                + Path.GetExtension(archivo.FileName);
+
+            if (EsImagen(archivo.ContentType))
+                GrabarImagen(archivo, nombreArchivo);
+            else
+                archivo.SaveAs(
+                    Path.Combine(Server.MapPath("~/archivos"),
+                               nombreArchivo));
+
+            /// poner el codigo de Grabar el archivo
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public ActionResult Multiples()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Multiples(IEnumerable<HttpPostedFileBase> archivos)
+        {
+            foreach (var archivo in archivos)
+            {
+                string nombreArchivo =
+                      archivo.FileName.ObtenerMD5()
+                      + Path.GetExtension(archivo.FileName);
+
+                if (EsImagen(archivo.ContentType))
+                    GrabarImagen(archivo, nombreArchivo);
+                else
+                    archivo.SaveAs(
+                        Path.Combine(Server.MapPath("~/archivos"),
+                                   nombreArchivo));
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool EsImagen(string contentType)
+        {
+            return contentType.Contains("image");
+        }
+
+        private void GrabarImagen(HttpPostedFileBase archivo, string nombreArchivo)
+        {
+            MemoryStream ms = new MemoryStream();
+            archivo.InputStream.CopyTo(ms);
+            Imagen imagen = new Imagen(ms, 
+                archivo.FileName, 
+                archivo.ContentType, 
+                Server.MapPath("~/archivos"));
+            imagen.Grabar(nombreArchivo, Server.MapPath("~/archivos/thumbnails"));
+        }
+    }
+}
---
+++ Controladores/Web.UI/Extensiones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class Extensiones
+    {
+        public static string ObtenerMD5(this string cadena)
+        {
+            MD5 md5 = MD5.Create();
+
+            byte[] hashData = md5.ComputeHash(Encoding.Default.GetBytes(cadena));
+
+            StringBuilder value = new StringBuilder();
+
+            for (int i = 0; i < hashData.Length; i++) value.Append(hashData[i].ToString());
+
+            return value.ToString();
+        }
+    }
+}
